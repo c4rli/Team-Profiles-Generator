9b56a41790092c3f8caded25ff78fadbee936306
xarli <117117311+c4rli@users.noreply.github.com>
2023-03-05 15:41:14 +0000

    Fixed switch case bug

--- index.js
+++ index.js
@@ -46,55 +46,99 @@ const specialQuestions = {
     }
 }
 
+
+const engineerQuestions = profileQuestions.slice()
+engineerQuestions.push(specialQuestions.engineer);
+
+const internQuestions = profileQuestions.slice()
+internQuestions.push(specialQuestions.intern);
+
+const managerQuestions = profileQuestions.slice()
+managerQuestions.push(specialQuestions.manager);
+
+
+
+const menuOptions = ["Add Engineer", "Add Intern", "Finish Building Team"];
+
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-inquirer.prompt(
-    profileQuestions
-).then(response => {
-    // promptForEngineer();
-    // populate manager info
-    promptForNextEmployee ()
-})
+// inquirer.prompt(
+//     profileQuestions
+// ).then(response => {
+//     // promptForEngineer();
+//     // populate manager info
+//     promptForNextEmployee ()
+// })
 
 const promptForNextEmployee = () => {
     inquirer.prompt([{
         name: "promptNext",
         type: "list",
         message: "Select:",
-        choices: ["Add engineer", "Add Intern", "Finish Building Team"]
-      }]).then(response => {
-        // if engineer
-        //    promptForEngineer
-        // else if intern
-        //    promptForIntern
-        // else
-        //    use the functionality from page-template to generate the team
+        choices: menuOptions
+    }]).then(response => {
+        displayMenu(response);
     })
+}
+
+
+function displayMenu(response) {
+    switch (response.promptNext) {
+        case menuOptions[0]: promptForEngineer();
+            break;
+        case menuOptions[1]: promptForIntern();
+            break;
+        case menuOptions[2]: finishBuildingTeam();
+            break;
+
+    }
 }
 
 const promptForEngineer = () => {
-    const engineerQuestions = profileQuestions.slice()
-    engineerQuestions.push(specialQuestions.engineer);
-
     inquirer.prompt(
         //engineer questions
+
         engineerQuestions
+        // internQuestions
 
     ).then(response => {
         // add new engineer to employees array
-        // promptForNextEmployee
+        promptForNextEmployee();
     })
 }
 
+
+
 const promptForIntern = () => {
-    inquirer.prompt([{
+
+    inquirer.prompt(
         //intern questions
-    }]).then(response => {
+        internQuestions
+
+    ).then(response => {
         // add new intern to employees array
-        // promptForNextEmployee
+        promptForNextEmployee();
+    })
+}
+
+const promptForManager = () => {
+
+    inquirer.prompt(
+        //manager questions
+        managerQuestions
+
+    ).then(response => {
+        // add new manager to employees array
+        promptForNextEmployee();
     })
 }
 
 const buildPage = () => {
     // render(myArrayOfTeamMembers)
 }
+
+function finishBuildingTeam() {
+
+}
+
+promptForManager();
